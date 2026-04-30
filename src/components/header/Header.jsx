e13d42288e3d961af410a90bcc3f306f4e2d8c3a
Vikindog <Bada9889@gmail.com>
2026-04-30 11:39:06 +0300

    Переделан размер фото на главном экране, раздел "Предложения" передалан, добавлен заголовок приветствия

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,14 @@
-import LOGO from '/saturn-logo.png'
-import './Header.css'
+import LOGO from "/saturn-logo.png";
+import "./Header.css";
 const Header = () => {
-    return <div className="header">
-                <div className="container header_container">
-                    <img className='saturn-logo' src={LOGO} alt="Логотип" />
-                    <h2 className="header-logo">Saturn</h2>
-                </div>
-            </div>
-}
- 
+  return (
+    <div className="header">
+      <div className="container header_container">
+        <img className="saturn-logo" src={LOGO} alt="Логотип" />
+        <h2 className="header-logo">Saturn</h2>
+      </div>
+    </div>
+  );
+};
+
 export default Header;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,33 +1,57 @@
 import SideBar from "/src/components/sidebar/SideBar.jsx";
-import MainImg from "/public/Main.webp"
-import "./Main.css"
+import MainImg from "/public/Main.webp";
+import "./Main.css";
 const Main = () => {
-    return <div className="main">
-                <div className="container main-container">
-                    <SideBar/>             
-                    <main className="content">
-                        <div className="content-bgi">
-                            <h1>Saturn | 4 Сезон</h1>
-                            <div className="server-ip">
-                                <span>Saturn.gomc.fun</span>
-                                <button> IP📎</button>
-                            </div>
-                            <img className="content-img" src={MainImg} alt="Фотография" />
-                        </div> 
-                        <p className="content-description">🌍 <span>Saturn</span> — это ванильный Minecraft сервер с элементами ролевой игры, где каждый игрок может найти своё место в огромном мире возможностей! 🎮✨<br /><br /><br />
-
-                            Что мы предлагаем: <br />
-
-                            🗺️ Открытый мир на версии Java 1.21+ с уникальной атмосферой и классическим игровым процессом. <br />
-                            🧑‍⚕️ Продуманные ролевые механики, где вы можете стать кем угодно — от президента до агента СБС, судьи или лидера собственного клана. <br />
-                            💿 Полезные плагины, которые улучшают игру, сохраняя её ванильный дух. <br />
-                            🏛️ Регулярные ивенты, турниры, ролевые взаимодействия и многое другое. <br />
-                            😂 Активное и дружелюбное сообщество, где вам всегда рады! <br />
-                            Присоединяйтесь к Saturn и начните свои приключения в мире, где все зависит только от ваших действий и фантазии! 🌟 <br />
-                        </p>
-                    </main>
-                </div>
+  return (
+    <div className="main">
+      <div className="container main-container">
+        <SideBar />
+        <main className="content">
+          <h2>Добро пожаловать!</h2>
+          <div className="content-bgi">
+            <h1>Saturn | 4 Сезон</h1>
+            <div className="server-ip">
+              <span>Saturn.gomc.fun</span>
+              <button> IP📎</button>
             </div>
-}
- 
+            <img className="content-img" src={MainImg} alt="Фотография" />
+          </div>
+          <p className="content-description">
+            🌍 <span>Saturn</span> — это ванильный Minecraft сервер с элементами
+            ролевой игры, где каждый игрок может найти своё место в огромном
+            мире возможностей! 🎮✨
+            <br />
+            <br />
+            <br />
+            Что мы предлагаем: <br />
+          </p>
+          <div className="offers">
+            <div className="offer">
+              <span>🗺️</span> Открытый мир на версии Java 1.21+ с уникальной атмосферой и
+              классическим игровым процессом.
+            </div>
+            <div className="offer">
+              <span>🧑‍⚕️</span> Продуманные ролевые механики, где вы можете стать кем угодно —
+              от президента до агента СБС, судьи или лидера собственного клана.
+            </div>
+            <div className="offer">
+              <span>💿</span> Полезные плагины, которые улучшают игру, сохраняя её ванильный
+              дух.
+            </div>
+            <div className="offer">
+              <span>🎮</span> Регулярные ивенты, турниры, ролевые взаимодействия и многое
+              другое.
+            </div>
+            <div className="offer">
+              <span>😂</span> Активное и дружелюбное сообщество, где вам всегда рады! <br />
+              Присоединяйтесь к Saturn и начните свои приключения в мире, где
+              все зависит только от ваших действий и фантазии!
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
 export default Main;
